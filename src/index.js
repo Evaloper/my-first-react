05bcd5414eb59c94e973b0d8d44ebd848199e355
Evaloper <gbajeeva@yahoo.com>
2022-10-30 23:54:20 +0100

    React code(JSX)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function ProfilePics() {
+    return (
+      <>
+        <img src="https://media-exp1.licdn.com/dms/image/C4D03AQHmu38XXGmsqQ/profile-displayphoto-shrink_200_200/0/1651077574377?e=1669852800&v=beta&t=liqI8fGbKeMOouBQQ84hOgJ5rD49q6szIBI9_0yuhJc" id="profile__img" alt="" />
+        <h4>Gbaje Eva</h4>
+      </>
+        )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// function Footer() {
+//     return (
+//         <footer>
+//         </footer>
+//     )
+// }
+
+function LinkSection() {
+    return (
+        <div>
+          <button>
+            <a href="twitter.com/fj_fabulous" id="twitter">Twitter</a>
+          </button>
+          <button>
+            <a href="https://training.zuri.team/" id="btn_zuri">Zuri Team</a>
+          </button>
+          <button>
+            <a href="http://books.zuri.team" id="books">Zuri Books</a>
+          </button>
+          <button>
+            <a href="https://books.zuri.team/python-for-beginners?ref_id=taddy" id="book__python">Python Books</a>
+          </button>
+          <button>
+            <a href="https://background.zuri.team" id="pitch">Background check for coders</a>
+          </button>
+          <button>
+            <a href="https://books.zuri.team/design-rules" id="book-design">Design Books</a>
+          </button>
+        </div>
+    )
+}
+
+function Page() {
+    return (
+        <div>
+            <ProfilePics />
+            <LinkSection />
+        </div>
+    )
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"))
