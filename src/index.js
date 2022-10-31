d16bb71e40d01dfafcc077a2bcff2b61fcc87de5
Evaloper <gbajeeva@yahoo.com>
2022-10-31 15:55:57 +0100

    Footer styling

--- src/index.js
+++ src/index.js
@@ -3,20 +3,14 @@ import ReactDOM from "react-dom"
 
 function ProfilePics() {
     return (
-      <>
+      <div id="profile">
         <img src="https://media-exp1.licdn.com/dms/image/C4D03AQHmu38XXGmsqQ/profile-displayphoto-shrink_200_200/0/1651077574377?e=1669852800&v=beta&t=liqI8fGbKeMOouBQQ84hOgJ5rD49q6szIBI9_0yuhJc" id="profile__img" alt="" />
         <h4>Gbaje Eva</h4>
         <h4 id="slack">Taddy</h4>
-      </>
+      </div>
         )
 }
 
-// function Footer() {
-//     return (
-//         <footer>
-//         </footer>
-//     )
-// }
 
 function LinkSection() {
     return (
@@ -39,15 +33,30 @@ function LinkSection() {
           <button>
             <a href="https://books.zuri.team/design-rules" id="book__design">Design Books</a>
           </button>
+          <div className="last_div">
+            <img className="slack" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Slack_icon_2019.svg/2048px-Slack_icon_2019.svg.png" alt="slack"/>
+            <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png"  alt="github" className="github"/>
+          </div>
         </div>
     )
 }
 
+function Footer() {
+  return (
+      <footer>
+        <div ><img src="https://internship.zuri.team/static/media/logo.36d2d48a.svg" className="footer-div"/></div>
+        <div><p>HNG Internship 9 Frontend Task</p></div>
+        <div ><img src="https://i0.wp.com/ingressive.org/wp-content/uploads/2020/05/I4G-Logo-Color-Cropped.png?fit=2225%2C539&ssl=1" className="footer-img"v/></div>
+      </footer>
+  )
+}
+
 function Page() {
     return (
-        <div>
+        <div id="main">
             <ProfilePics />
             <LinkSection />
+            <Footer />
         </div>
     )
 }
